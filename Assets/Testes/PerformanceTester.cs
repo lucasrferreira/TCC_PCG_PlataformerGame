99dad8f29253c221678b7c6d85220ce339b03829
Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
2017-05-23 23:24:37 -0300

    Some performance testes and test tiles logic

---
+++ Assets/Testes/PerformanceTester.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerformanceTester : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    public List<Sprite> sprites;
+
+    public int countOfObjects;
+	// Use this for initialization
+	void Start ()
+	{
+	    StartCoroutine(SpawGameObjects());
+	}
+
+    IEnumerator SpawGameObjects()
+    {
+        var instatiated = prefab;
+        for (int index = 0; index < countOfObjects; index++)
+        {
+            instatiated = Instantiate(prefab, transform, false);
+            instatiated.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+            yield return null;
+        }
+        yield return null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
